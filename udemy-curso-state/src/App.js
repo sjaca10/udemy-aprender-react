356c4964dabc2af3c3e194db420d2e871a81e092
Javier Aguila <sjaca10@gmail.com>
2020-09-21 12:20:33 -0500

    Update state with setState()

--- udemy-curso-state/src/App.js
+++ udemy-curso-state/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// React es declarativo y reactivo (en gran medida al state):
+// - declaractivo porque se le dice qué se debe renderizar y no cómo debe renderizar
+// - reactivo porque reacciona a los cambios de los componentes
 class Counter extends Component {
   constructor() {
     super()
@@ -8,6 +11,16 @@ class Counter extends Component {
     this.state = {
       counter: 1
     }
+
+    // No se debe mutar el estado de un componente directamente porque React
+    // optimiza cuando realizar los cambios en el árbol de elementos DOM 
+    // a través de una cola priorizada para reflejar los cambios de los componentes.
+    // De mutar directamente, React no podría hacer esa optimización
+    // Se debe user setState (async method -> no se sabe hasta que punto se hara el cambio del estado)
+    // setInterval(() => this.state.counter = this.state.counter + 1)
+    setInterval(() => {
+      this.setState({ counter: this.state.counter + Math.random() })
+    }, 1000)
   }
 
   render() {
