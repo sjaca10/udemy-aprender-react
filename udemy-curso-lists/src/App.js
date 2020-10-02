6015a5ada9cb19527b1ebb7521f4114429a1b4b6
Javier Aguila <sjaca10@gmail.com>
2020-10-01 22:32:09 -0500

    Transform array in React list and use key property

--- udemy-curso-lists/src/App.js
+++ udemy-curso-lists/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const numbers = [1, 1, 3, 4, 5]
+    
+    return (
+      <div className="App">
+        <h4>Trabajando con listas</h4>
+        {numbers.map((n, i) => {
+          return <p key={i}>Soy el número {n}</p>
+        })}
+      </div>
+    )
+  }
 }
 
 export default App;
