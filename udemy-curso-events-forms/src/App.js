5efeeab0632fb89affa70843126f2beca0a5edd6
Javier Aguila <sjaca10@gmail.com>
2020-10-30 20:31:49 -0600

    Synthetic Event

--- udemy-curso-events-forms/src/App.js
+++ udemy-curso-events-forms/src/App.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react'
 import './App.css';
 
 class App extends Component {
+  handleClick(e) {
+    console.log(e) // Evento sintetico del React para proveer compatibildiad
+    console.log(e.nativeEvent) // Evento nativo del navegador
+    alert('Hi here!')
+  }
+
   render() {
     return (
     <div className="App">
       <h4>Eventos</h4>
-      <button onClick={() => alert('Hi here!')}>Hi there!</button>
+      {/* Example defining anonymous function to execute */}
+      {/* <button onClick={() => alert('Hi here!')}>Hi there!</button> */}
+      {/* Example calling a function */}
+      <button onClick={this.handleClick}>Hi there!</button>
     </div>
     );
   }
