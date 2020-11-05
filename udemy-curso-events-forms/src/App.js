d9438e07b1c7bf723f99810c927c12303983e714
Javier Aguila <sjaca10@gmail.com>
2020-11-04 20:12:55 -0600

    Supported events in React, onMouseMove example, context linking

--- udemy-curso-events-forms/src/App.js
+++ udemy-curso-events-forms/src/App.js
@@ -2,10 +2,31 @@ import React, { Component } from 'react'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      mouseX: 0,
+      mouseY: 0,
+    }
+
+    // Link context to the method, required to avoid TypeError: Cannot read property setState of null
+    // Old, ugly, dirty way, better use arrow function
+    // this.handleMouseMove = this.handleMouseMove.bind(this)
+  }
+
   handleClick(e) {
-    console.log(e) // Evento sintetico del React para proveer compatibildiad
-    console.log(e.nativeEvent) // Evento nativo del navegador
+    console.log(e) // Synthetic event from React to provide compatiblity
+    console.log(e.nativeEvent) // Browser's native event
     alert('Hi here!')
+  }
+
+  // Link context to the method, required to avoid TypeError: Cannot read property setState of null
+  // Recommended, cleaner method, through an arrow function
+  // handleMouseMove(e) {
+  handleMouseMove = (e) => {
+    const { clientX, clientY } = e
+    this.setState({ mouseX: clientX, mouseY: clientY })
   }
 
   render() {
@@ -16,6 +37,13 @@ class App extends Component {
       {/* <button onClick={() => alert('Hi here!')}>Hi there!</button> */}
       {/* Example calling a function */}
       <button onClick={this.handleClick}>Hi there!</button>
+      <div 
+        onMouseMove={this.handleMouseMove}
+        style={{ border: '1px solid #000', marginTop: 10, padding: 10 }}>
+        <p>
+          {this.state.mouseX}, {this.state.mouseY}
+        </p>
+      </div>
     </div>
     );
   }
