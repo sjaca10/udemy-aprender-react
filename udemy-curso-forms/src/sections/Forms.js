c8b84df6fc60f848ec1cf917aa3ab7addc165806
Javier Aguila <sjaca10@gmail.com>
2020-11-25 22:18:10 -0600

    Recommend approach to use forms in React: controled elements

--- udemy-curso-forms/src/sections/Forms.js
+++ udemy-curso-forms/src/sections/Forms.js
@@ -1,50 +1,63 @@
 import React, { Component } from 'react'
 
 export default class Forms extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            inputName: '',
+            inputTwitter: '@',
+            inputTerms: true
+        }
+    }
+
     handleSubmit = (e) => {
         e.preventDefault()
-        const name = this.inputName.value
-        const twitter = this.inputTwitter.value
-        console.log({name, twitter})
+        console.log(this.state)
     }
 
     handleChange = (e) => {
         console.log('handleChange', e.target.checked)
+        this.setState({ inputTerms: e.target.checked })
     }
 
     render() {
         return (
-        <div>
-            <h4>Formularios</h4>
-            <form onSubmit={this.handleSubmit}>
-                <p>
-                    <label htmlFor='name'> Name: </label>
-                    <input
-                        id='name'
-                        name='userName'
-                        placeholder='Introduce tu nombre'
-                        ref={inputElement => this.inputName = inputElement} />
-                </p>
-                <p>
-                    <label htmlFor='twitter'>Twitter: </label>
-                    <input
-                        id='twitter'
-                        name='twitterAccount'
-                        placeholder='Introduce tu Twitter'
-                        ref={inputElement => this.inputTwitter = inputElement} />
-                </p>
-                <p>
-                    <label htmlFor='termsConditions'>Terms and Conditions</label>
-                    <input
-                        id='termsConditions'
-                        name='termsAndConditions'
-                        type='checkbox'
-                        onChange={this.handleChange}
-                    />
-                </p>
-                <button>Enviar</button>
-            </form>
-        </div>
+            <div>
+                <h4>Formularios</h4>
+                <form onSubmit={this.handleSubmit}>
+                    <p>
+                        <label htmlFor='name'> Name: </label>
+                        <input
+                            id='name'
+                            name='userName'
+                            placeholder='Introduce tu nombre'
+                            value={this.state.inputName} 
+                            onChange={e => this.setState({ inputName: e.target.value })}
+                        />
+                    </p>
+                    <p>
+                        <label htmlFor='twitter'>Twitter: </label>
+                        <input
+                            id='twitter'
+                            name='twitterAccount'
+                            placeholder='Introduce tu Twitter'
+                            value={this.state.inputTwitter}
+                            onChange={e => this.setState({ inputTwitter: e.target.value })}
+                        />
+                    </p>
+                    <p>
+                        <label htmlFor='termsConditions'>Terms and Conditions</label>
+                        <input
+                            id='termsConditions'
+                            name='termsAndConditions'
+                            type='checkbox'
+                            onChange={this.handleChange} 
+                            checked={this.state.inputTerms} />
+                    </p>
+                    <button>Enviar</button>
+                </form>
+            </div>
         )
     }
 }
