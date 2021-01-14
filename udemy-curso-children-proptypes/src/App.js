aa452eee18c76b9f28db9f71def7aad17c47cef6
Javier Aguila <sjaca10@gmail.com>
2021-01-14 17:45:04 -0600

    Prop Children

--- udemy-curso-children-proptypes/src/App.js
+++ udemy-curso-children-proptypes/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Box extends Component {
+  render() {
+    return (
+      <div style={{ border: '1px solid #09f', margin: 5, padding: 5 }}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h4>Children props</h4>
+        <Box>Hola! Soy un children!</Box>
+        <Box>Otro Box con otro contenido</Box>
+      </div>
+    );
+  }
 }
 
 export default App;
