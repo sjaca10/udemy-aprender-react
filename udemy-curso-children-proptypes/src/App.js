74735883d3ebf4a6cdec3a32e504af04a9863e38
Javier Aguila <sjaca10@gmail.com>
2021-01-14 17:52:25 -0600

    Children layout

--- udemy-curso-children-proptypes/src/App.js
+++ udemy-curso-children-proptypes/src/App.js
@@ -10,13 +10,47 @@ class Box extends Component {
   }
 }
 
+class Article extends Component {
+  render() {
+    return (
+      <section>
+        <h2>{this.props.title}</h2>
+        <p><em>Escrito por {this.props.author}</em></p>
+        <Box>{this.props.date}</Box>
+        <article>
+          {this.props.children}
+        </article>
+      </section>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <h4>Children props</h4>
-        <Box>Hola! Soy un children!</Box>
-        <Box>Otro Box con otro contenido</Box>
+        <Article
+          author='Javier'
+          date={new Date().toLocaleDateString()}
+          title='Artículo sobre la prop children'>
+            <p>El contenido que envolvemos dentro del componente Article será enviado al componente como this.props.children</p>
+            <strong>Y mantiene las etiquetas y componentes que hayáis añadido dentro</strong>
+        </Article>
+        <Article
+          author='Javier'
+          date={new Date().toLocaleDateString()}
+          title='Artículo 2'>
+            <p>El contenido que envolvemos dentro del componente Article será enviado al componente como this.props.children</p>
+            <strong>Y mantiene las etiquetas y componentes que hayáis añadido dentro</strong>
+        </Article>
+        <Article
+          author='Javier'
+          date={new Date().toLocaleDateString()}
+          title='Otro artículo'>
+            <p>El contenido que envolvemos dentro del componente Article será enviado al componente como this.props.children</p>
+            <strong>Y mantiene las etiquetas y componentes que hayáis añadido dentro</strong>
+        </Article>
       </div>
     );
   }
