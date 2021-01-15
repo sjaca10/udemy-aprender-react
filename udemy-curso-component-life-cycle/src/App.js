c8955d920313ffd6c28152a2b833ee10c8d0cf9d
Javier Aguila <sjaca10@gmail.com>
2021-01-15 16:10:45 -0600

    Component life cycle: mount - constructor

--- udemy-curso-component-life-cycle/src/App.js
+++ udemy-curso-component-life-cycle/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Default constructor
+  // constructor (...args) {
+  //   super(...args)
+  // }
+
+  constructor (props) {
+    console.log('constructor');
+    super(props); // This method calls Component construct method
+
+    // Initialize the component state
+    this.state = {
+      initialMessage: 'Initial message'
+    }
+
+    // Bind context to method, old approach, better use arrow functions
+    // this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = () => {
+    this.setState({ initialMessage: 'Changed message '})
+  }
+
+  render() {
+    console.log('render')
+    return (
+      <div className="App">
+        <h4>Ciclo de montaje: constructor</h4>
+        {this.state.initialMessage}
+        <button onClick={this.handleClick}>
+          Change message
+        </button>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
